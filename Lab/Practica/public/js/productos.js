cc08f5ab8669c21b7dc0a8cbb1a0bb4990fdd1b3
Alejandro <alejandro.garcia.dlf@gmail.com>
2023-04-03 11:54:57 +0200

    Animation for products changed

--- Lab/Practica/public/js/productos.js
+++ Lab/Practica/public/js/productos.js
@@ -1,28 +1,33 @@
 Cargar('./html/overworld.html', 'productos_section');
 
 function cambiarTab(event) {
-let tab = {
-    "overworld-a":"./html/overworld.html",
-    "nether-a": "./html/nether.html",
-    "end-a":"./html/end.html"
-}
-
-let id = event.target.id;
-
-Cargar(tab[id], "productos_section");
-
-// Remove active class for all instances of the div.
-let div_row = document.getElementById("pseudoheader-row");
-
-for(let i = 0; i < div_row.childNodes.length; i++){
-    let div = div_row.childNodes[i];
-    try{
-    if (div.classList.contains("active")){
-        div.classList.remove("active");
-        div.removeChild(document.getElementById("steve-indicator"));
+    const tab = {
+        "overworld-a":"./html/overworld.html",
+        "nether-a": "./html/nether.html",
+        "end-a":"./html/end.html"
     }
-    }catch(smt){}
-}
-event.target.parentElement.classList.add("active");
-event.target.parentElement.innerHTML += '<img id="steve-indicator" class="steve-face" src="./resources/steve-face.png" alt=".."/>';
+
+    const position = {
+        "nether-a" : -1,
+        "overworld-a" : 0,
+        "end-a" : 1
+    }
+
+    let id = event.target.id;
+
+    Cargar(tab[id], "productos_section");
+
+    // Remove active class for all instances of the div.
+    document.getElementById("wrapper").childNodes.forEach((item) => {
+        try {
+            item.classList.remove("active");
+        }catch(smt){}
+    });
+
+    event.target.parentElement.classList.add('active');
+
+    let width = event.target.parentElement.offsetWidth;
+    
+    document.getElementById("steve-indicator")
+        .style.transform = "translateX(" + ((width + 5) * position[id]) + "px)";
 }
